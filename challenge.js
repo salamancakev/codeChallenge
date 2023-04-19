66f39170e1869431953032170a11fd964df450d4
Kevin Salamanca <kevinsalamanca26@gmail.com>
2023-04-19 11:48:42 -0400

    Code challenge

--- challenge.js
+++ challenge.js
@@ -1,20 +1,29 @@
-function compareFn(a, b) {
-  if (a < b) {
-    return -1;
+function calculateMinSum(coinsArray = []) {
+  coinsArray.sort((a, b) => a - b);
+  let sum = 0;
+  for (let i = 0; i < coinsArray.length; i++) {
+    if (coinsArray[i] > sum + 1) {
+      return sum + 1;
+    }
+    sum = sum + coinsArray[i];
   }
-  if (a > b) {
-    return 1;
-  }
-  return 0;
+  return sum + 1;
 }
 
-function calculateMinSum(coinsArray = []){
-coinsArray.sort(compareFn);
-let sum = 0;
-for (let i = 0; i < coinsArray.length; i++) {
-sum = sum + coinsArray[i];
-if(sum < coinsArray[]){
+// Test Case 1
+let coins = [5, 7, 1, 1, 2, 3, 22];
+let result = calculateMinSum(coins);
+console.log("Test case 1");
+console.log("Result: " + result);
 
-}
-}
-}
+// Test Case 2
+coins = [1, 1, 1, 1, 1];
+result = calculateMinSum(coins);
+console.log("Test case 2");
+console.log("Result: " + result);
+
+// Test Case 3
+coins = [1, 5, 1, 1, 1, 10, 15, 20, 100];
+result = calculateMinSum(coins);
+console.log("Test case 3");
+console.log("Result: " + result);
